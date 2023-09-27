d0b8a19a19404c05a493bd865ea26cc53404d7fc
Alexandre Moura <alexandremoura293rgmail.com>
2023-09-27 19:06:33 -0300

    GURDANDO AS LETRAS ACERTADAS

--- forca.py
+++ forca.py
@@ -2,11 +2,25 @@ def jogar():
     print("JOGO DA FORCA")
     
     palavra_secreta = "banana"
+    
+    letras_acertadas = ["_","_","_","_","_","_"]
 
     enforcou = False
     acertou = False
 
     while(not enforcou and not acertou):
+        
+        chute = (input("Qual letra? ")).strip()
+        
+        posicao = 0
+        
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                letras_acertadas[posicao] = letra
+                (letra, posicao)
+                print(letras_acertadas)
+            posicao = posicao + 1    
+
         print("JOGANDO")
 
 if(__name__ == "__main__"):
